7e376332f7431f5e8dce7c9df35dfd6fa004f50b
kyh <kyh_0625@naver.com>
2024-02-23 03:04:30 +0900

    최근문서내역

--- front/README.md
+++ front/README.md
@@ -1,4 +1,6 @@
-사이트 기본정보설정 = basic_info
+사이트 기본정보설정 = Basic_info
 최근 문의내역 현황 = FAQ_list
-최근 댓글 현황 = comment_list
-최근 게시글 등록현황 = board_list
+최근 댓글 현황 = Comment_list
+최근 게시글 등록현황 = Board_list
+
+<!-- layout에 NextProvider해야함 -->
--- front/src/app/components/FAQ_list.tsx
+++ front/src/app/components/FAQ_list.tsx
@@ -1,9 +1,79 @@
-import { Button } from 'antd';
+import type { TableProps } from 'antd';
+import { Button, Table } from 'antd';
+
+interface DataType {
+  key: string;
+  info: string;
+  date: string;
+  status: any;
+  tool: any;
+}
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: '접수정보',
+    dataIndex: 'info',
+    key: 'info',
+  },
+  {
+    title: '접수일',
+    dataIndex: 'date',
+    key: 'date',
+  },
+  {
+    title: '처리상태',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: '관리자도구',
+    dataIndex: 'tool',
+    key: 'tool',
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    info: 'John Brown',
+    date: new Date().toISOString(),
+    status: '접수중',
+    tool: <Button>보기</Button>
+  },
+  {
+    key: '2',
+    info: 'John Brown',
+    date: new Date().toISOString(),
+    status: '접수중',
+    tool: <Button>보기</Button>
+  },
+  {
+    key: '3',
+    info: 'John Brown',
+    date: new Date().toISOString(),
+    status: '접수중',
+    tool: <Button>보기</Button>
+  },
+  {
+    key: '4',
+    info: 'John Brown',
+    date: new Date().toISOString(),
+    status: '접수중',
+    tool: <Button>보기</Button>
+  },
+];
 
 export default function FAQ_list() {
   return (
     <div>
-      <div className="flex">
+      <Table columns={columns} dataSource={data} pagination={false} />
+    </div>
+  );
+}
+
+/**
+ * 
+ * <div className="flex">
         <div>로고</div>
         <div className="text-xl">최근 문의내역 현황</div>
         <Button className="float-right">더보기</Button>
@@ -14,12 +84,12 @@ export default function FAQ_list() {
         <div className="grow-[1]">처리상태</div>
         <div className="grow-[1]">관리자툴</div>
       </div>
-      <div>
-        <div>건축상담</div>
-        <div>2024-02-23</div>
-        <div>접수중</div>
-        <div>보기</div>
+      <div className="p-4 flex">
+        <div className="grow-[2] border-slate-400 border-[1px]">건축상담</div>
+        <div className="grow-[1] border-slate-400 border-[1px]">2024-02-23</div>
+        <div className="grow-[1] border-slate-400 border-[1px]">접수중</div>
+        <div className="grow-[1] border-slate-400 border-[1px]">보기</div>
       </div>
     </div>
-  );
-}
+ * 
+ */
